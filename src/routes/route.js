beacef438877d043a3310b5274ad7049c7566b94
mdyousuf <yousuf9007@gmail.com>
2022-08-08 18:35:23 +0530

    assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -35,4 +35,69 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+//problem 1
+router.get('/movies',function(req,res){
+    let moviesList =["Rang de basanti", "The shining", "Lord of the rings"," Batman begins"]
+res.send(moviesList)
+
+})
+
+//problem 2 and problem 3
+router.get('/movies/:index',function(req,res){
+    const movies=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins','The Shining',"Titanic","Shutter Island","Pans Labyrinth","Wrath of Titans","Edge of Tommorrow"];
+    const value=req.params.index;
+     
+    if(value<10){
+        res.send(movies[value]);
+    }else{
+        res.send("Index Number Does not Exist");
+    }
+});
+//problem 4
+router.get('/films',function(req,res){
+   const films = [ {
+        "id": 1,
+        "name": "“The Shining”"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+    
+    res.send(films)       
+
+})
+//problem 5
+router.get("/films/:filmId",function(req,res){
+    let value=req.params.filmId;
+    const film=[ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }];
+       if(value<film.length){
+        res.send(film[value])
+       }else{
+        res.send("No movie exists with this id")
+            
+       }
+  
+       
+});
+
+
+
 module.exports = router;
