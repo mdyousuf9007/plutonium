a74d819d524293e96c0b5a0c24c1eed13b1ced22
mdyousuf <yousuf9007@gmail.com>
2022-09-01 10:58:17 +0530

    assignment completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,287 @@
-const UserModel= require("../models/userModel")
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel");
 
+const createUser = async function (req, res) {
+  try {
+    let data = req.body;
+    if (Object.keys(data).length != 0) {
+      let savedData = await userModel.create(data)
+      res.status(201).send({ msg: savedData })
+    }
+    else res.status(400).send({ msg: "BAD REQUEST" })
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  }
+}
 
+const loginUser = async function (req, res) {
+  try {
+    let data = req.body
+    if (Object.keys(data).length != 0) {
+      let user = await userModel.findOne({ emailId: data.emailId, password: data.password });
+      if (!user)
+        return res.status(403).send({
+          msg: "username or the password is not corerct",
+        });
+      let token = jwt.sign(
+        {
+          userId: user._id.toString(),
+          batch: "plutonium",
+          organisation: "FUnctionUp",
+        },
+        "yousufkhan"
+      );
+      res.status(201).send({ status: true, token: token });
+    }
+    else {
+      res.status(400).send({ msg: "BAD REQUEST" })
+    }
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  };
+}
 
+const getUserData = async function (req, res) {
+  try {
+    let userId = req.params.userId;
 
-const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
+    let userDetails = await userModel.findById(userId);
+    if (!userDetails) {
+      return res.status(401).send({ status: false, msg: "No such user exists" });
+    }
+    res.send({ status: true, data: userDetails });
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  };
+}
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+// const postMessage = async function (req, res) {
+//     let message=req.body.message
+//     let user=await userModel.findById(req.params.userId)
+//     if(!user) return res.send({msg:"no such user exists"})
+//     let updatedPosts=user.posts 
+//      updatedPosts.push(message)
+//     let updatedUser=await userModel.findOneAndUpdate({_id:user._id},{posts:updatedPosts},{new:true})
+//      return res.send({status:true,data:updatedUser})
+
+//   }
+
+const updateUser = async function (req, res) {
+
+  try {
+
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(401).send({ status: false, msg: "No such user exists" });
     }
 
+    let userData = req.body;
+    if (Object.keys(userData).length != 0) {
+      let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+      res.status(200).send({ status: true, data: updatedUser });
+    }
+    else {
+      res.status(400).send({ msg: "BAD REQUEST" })
+    }
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  };
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
+const deleteUser = async function (req, res) {
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
-module.exports.basicCode= basicCode
+  try {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(401).send({ status: false, msg: "No such user exists" });
+    }
+
+    let deletedUser = await userModel.findOneAndUpdate(
+      { _id: userId },
+      { $set: { isDeleted: true } },
+      { new: true }
+    );
+
+    res.status(204).send({ status: true, data: deletedUser, });
+  }
+  catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+  };
+}
+module.exports.createUser = createUser;
+module.exports.getUserData = getUserData;
+module.exports.updateUser = updateUser;
+module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
+// module.exports.postMessage=postMessage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const UserModel= require("../models/userModel")
+// const basicCode= async function(req, res) {
+//     let tokenDataInHeaders= req.headers.token
+//     console.log(tokenDataInHeaders)
+
+//     console.log( "HEADER DATA ABOVE")
+//     console.log( "hey man, congrats you have reached the Handler")
+//     res.send({ msg: "This is coming from controller (handler)"})
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
+// module.exports.basicCode= basicCode
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
@@ -5,27 +6,122 @@ const userSchema = new mongoose.Schema( {
     lastName: String,
     mobile: {
         type: String,
-        unique: true,
+
         required: true
     },
     emailId: String,
+    password: String,
     gender: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        enum: ["male", "female", "other"]
+    },
+    isDeleted: {
+        type:Boolean,
+        default:false
     },
     age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    posts:{
+        type:[],default:[]
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('User', userSchema)
 
 
 
-// String, Number
-// Boolean, Object/json, array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
+//     // isIndian: Boolean,
+//     // parentsInfo: {
+//     //     motherName: String,
+//     //     fatherName: String,
+//     //     siblingName: String
+//     // },
+//     // cars: [ String  ]
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema) //users
+
+
+
+// // String, Number
+// // Boolean, Object/json, array
